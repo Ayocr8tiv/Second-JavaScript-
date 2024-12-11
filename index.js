8bcbc495482d0022536423e72be239824a18f035
Ayo <ayocre8tiv@gmail.com>
2024-12-11 12:15:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+var developer = 'Ayo'
+var designer = "Sydney"
+var accountant = 'Damola'
+
+// Concatenation
+
+var sentence = `I have a friends named ${developer} is a developer and a designer named ${designer} with an accountant named ${accountant}`;
+console.log(sentence);
+
+console.log(sentence.length);
+console.log(sentence.toUpperCase());
+console.log(sentence.toLowerCase());
+
+
+var age = 25
+if(age >= 18){
+    console.log("you can vote");
+    
+} else{
+    console.log("You're too young vote");
+    
+}
+
+var students = false
+
+if(students == true){
+    console.log("free ticket");
+    
+} else{
+    console.log("pay your complete money");
+    
+}
+
+var me = 'tired'
+
+if(me != "tired"){
+    console.log('contiune working');
+    
+}else{
+    console.log('take a break');
+    
+}
+
+var presido = "Tpain"
+if(presido != "Tpain"){
+    console.log("life is good");
+    
+}else{
+    console.log("sapa dey for here");
+    
+}
+
+var jobTittle = 'Designer'
+if(jobTittle == 'Lawyer'){
+    console.log('Your bill is 200k');
+    
+}else if(jobTittle == 'Police'){
+    console.log('Your bill is 1 millon');
+    
+}else if(jobTittle == 'Influencer'){
+    console.log('Your bill is 600k');
+    
+}else if(jobTittle == 'Instructor'){
+    console.log('Your bill is 50k');
+    
+}else{
+    console.log('Your bill is 100k');
+    
+}
+
